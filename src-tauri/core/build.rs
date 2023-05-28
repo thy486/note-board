8190f46a677c1868f9567dde3925a03febe1c2f2
thy486 <83344148+thy486@users.noreply.github.com>
2023-05-29 00:12:57 +0800

    chore: optimize the project development environment; optimize the build.rs script

--- src-tauri/core/build.rs
+++ src-tauri/core/build.rs
@@ -27,30 +27,33 @@ fn watch_wits() -> std::io::Result<()> {
     let mut allowed_ts_files = HashSet::new();
     for wit_entry in wit_read_dir.flatten() {
         let wit_path = wit_entry.path();
-        let iface =
+        if let Ok(iface) =
             wit_parser::parse_and_resolve_str(&fs::read_to_string(wit_path).unwrap(), |_| false)
-                .expect("wit file error");
-        let rs_builder = RsHostBuilder {
-            fmt: true,
-            tracing: true,
-            async_: false,
-        };
-        let rs_general_contents = rs_builder.build(iface.clone()).to_file().1;
-        let mut rs_mod_code =
-            String::with_capacity(RS_ATTR_HEADERS.len() + rs_general_contents.len());
-        rs_mod_code.push_str(RS_ATTR_HEADERS);
-        rs_mod_code.push_str(&rs_general_contents);
-        rs_wit_codes.push(rs_mod_code);
-        let ts_builder = TsBuilder::default();
-        let (ts_file_name, ts_file_contents) = ts_builder.build(iface).to_file();
-        let mut ts_file_code =
-            String::with_capacity(TS_FILE_HEADERS.len() + ts_file_contents.len());
-        ts_file_code.push_str(TS_FILE_HEADERS);
-        ts_file_code.push_str(&ts_file_contents);
-        write_file(ts_dir_path.join(&ts_file_name), ts_file_code)?;
-        allowed_ts_files.insert(ts_file_name.to_owned());
+        {
+            let rs_builder = RsHostBuilder {
+                fmt: true,
+                tracing: true,
+                async_: false,
+            };
+            let rs_general_contents = rs_builder.build(iface.clone()).to_file().1;
+            let mut rs_mod_code =
+                String::with_capacity(RS_ATTR_HEADERS.len() + rs_general_contents.len());
+            rs_mod_code.push_str(RS_ATTR_HEADERS);
+            rs_mod_code.push_str(&rs_general_contents);
+            rs_wit_codes.push(rs_mod_code);
+            let ts_builder = TsBuilder::default();
+            let (ts_file_name, ts_file_contents) = ts_builder.build(iface).to_file();
+            let mut ts_file_code =
+                String::with_capacity(TS_FILE_HEADERS.len() + ts_file_contents.len());
+            ts_file_code.push_str(TS_FILE_HEADERS);
+            ts_file_code.push_str(&ts_file_contents);
+            write_file(ts_dir_path.join(&ts_file_name), ts_file_code)?;
+            allowed_ts_files.insert(ts_file_name.to_owned());
+        }
     }
-    write_file(WIT_FILE_RS_PATH, rs_wit_codes.join("\n\n"))?;
+    if !rs_wit_codes.is_empty() {
+        write_file(WIT_FILE_RS_PATH, rs_wit_codes.join("\n\n"))?;
+    }
     let wit_ts_read_dir = fs::read_dir(WIT_TS_DIR_PATH)?;
     for ts_file_entry in wit_ts_read_dir.flatten() {
         if !allowed_ts_files.contains(&PathBuf::from(ts_file_entry.file_name())) {
